3bf375790598d2855971423cc307b66b1e175037
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2023-07-08 07:14:27 +0800

    alvistack/v2023.07.1
        git clean -xdf
        tar zcvf ../python-jsonschema-specifications_2023.07.1.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-jsonschema-specifications.spec ../python-jsonschema-specifications_2023.07.1-1.spec
        cp ../python*-jsonschema-specifications*2023.07.1*.{gz,xz,spec,dsc} /osc/home\:alvistack/python-jsonschema-jsonschema-specifications-2023.07.1/
        rm -rf ../python*-jsonschema-specifications*2023.07.1*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='jsonschema-specifications',
+    version='2023.7.1',
+    description='The JSON Schema meta-schemas and vocabularies, exposed as a Registry',
+    author='Julian Berman',
+    author_email='Julian+jsonschema-specifications@GrayVines.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: File Formats :: JSON :: JSON Schema',
+    ],
+    install_requires=[
+        'importlib-resources>=1.4.0; python_version < "3.9"',
+        'referencing>=0.28.0',
+    ],
+    packages=[
+        'jsonschema_specifications',
+    ],
+    include_package_data=True,
+)
